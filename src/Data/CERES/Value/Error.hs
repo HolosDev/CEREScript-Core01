9a3c3072c66f227ea6cd36fb13fc5b59df6b4f35
YongJoon Joe <developer@quietjoon.net>
2020-03-08 15:04:18 +0900

    Refactoring Error generator with TextShow Builder

--- src/Data/CERES/Value/Error.hs
+++ src/Data/CERES/Value/Error.hs
@@ -12,16 +12,15 @@ import           Data.CERES.Value
 errValueWith2 :: (TextShow a, TextShow b) => Name -> Message -> a -> b -> Value
 errValueWith2 funcName errorType vA vB = ErrValue errorMessage
  where
-  errorMessage = TL.concat
-    [ "[Error]<"
-    , funcName
-    , " :=: "
-    , errorType
-    , "> "
-    , showtl vA
-    , " and "
-    , showtl vB
-    ]
+  errorMessage = toLazyText $
+    fromLazyText "[Error]<"
+    <> fromLazyText funcName
+    <> fromLazyText " :=: "
+    <> fromLazyText errorType
+    <> fromLazyText "> "
+    <> showb vA
+    <> fromLazyText " and "
+    <> showb vB
 
 errValueTEWith2 :: (TextShow a, TextShow b) => Name -> a -> b -> Value
 errValueTEWith2 funcName = errValueWith2 funcName "TypeError"
