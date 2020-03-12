36fa8ee2d60c3954d191746a656cfbd714798a5f
YongJoon Joe <developer@quietjoon.net>
2020-03-12 10:47:51 +0900

    Remove interpreter dependent functions `modify*`

--- src/CERES/Operate.hs
+++ src/CERES/Operate.hs
@@ -74,25 +74,7 @@ convertValue vA@(ErrValue _  ) (ErrValue _) = vA
 convertValue vA (ErrValue _) = ErrValue . showRawTL $ vA
 convertValue vA vB = errValueTEWith2 "convertValue" vA vB
 
-modifyValue :: CERESOperator -> IM.IntMap Value -> Value -> Value
-modifyValue operator localEnv value = newValue
- where
-  mOperand = error "[FIXME]<modifyValue> Use second argument"
-  newValue = maybe (ErrValue "Null register 0")
-                   (operatorSelector operator value)
-                   mOperand
-
 operatorSelector :: CERESOperator -> (Value -> Value -> Value)
 operatorSelector operator = case operator of
   COAMul -> coaMul
   _      -> error "No such operator"
-
-modifyValueStack :: [CERESOperator] -> IM.IntMap Value -> Value -> (IM.IntMap Value , Value)
-modifyValueStack []                     localEnv value = (localEnv, value)
-modifyValueStack (operator : operators) localEnv value = modifyValueStack
-  operators
-  newLocalEnv
-  newValue
- where
-  newValue    = error "[FIXME]<modifyValueStack> Not yet implemented"
-  newLocalEnv = error "[FIXME]<modifyValueStack> Not yet implemented"
