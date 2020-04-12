835f32ea82771626a1efd840fe06d897f64eefa0
YongJoon Joe <developer@quietjoon.net>
2020-04-13 02:41:31 +0900

    Add more util functions for showing `CERES`

--- src/Data/CERES/Util.hs
+++ src/Data/CERES/Util.hs
@@ -19,3 +19,82 @@ showb3 n a b c =
     <> showb b
     <> singleton ':'
     <> showb c
+
+showbCS1 :: TextShow a => Text -> a -> Builder
+showbCS1 n a = fromLazyText n <> singleton ' ' <> showb a
+showbCS2 :: (TextShow a, TextShow b) => Text -> a -> b -> Builder
+showbCS2 n a b =
+  fromLazyText n <> singleton ' ' <> showb a <> singleton ' ' <> showb b
+showbCS3
+  :: (TextShow a, TextShow b, TextShow c) => Text -> a -> b -> c -> Builder
+showbCS3 n a b c =
+  fromLazyText n
+    <> singleton ' '
+    <> showb a
+    <> singleton ' '
+    <> showb b
+    <> singleton ' '
+    <> showb c
+showbCS4
+  :: (TextShow a, TextShow b, TextShow c, TextShow d)
+  => Text
+  -> a
+  -> b
+  -> c
+  -> d
+  -> Builder
+showbCS4 n a b c d =
+  fromLazyText n
+    <> singleton ' '
+    <> showb a
+    <> singleton ' '
+    <> showb b
+    <> singleton ' '
+    <> showb c
+    <> singleton ' '
+    <> showb d
+showbCS5
+  :: (TextShow a, TextShow b, TextShow c, TextShow d, TextShow e)
+  => Text
+  -> a
+  -> b
+  -> c
+  -> d
+  -> e
+  -> Builder
+showbCS5 n a b c d e =
+  fromLazyText n
+    <> singleton ' '
+    <> showb a
+    <> singleton ' '
+    <> showb b
+    <> singleton ' '
+    <> showb c
+    <> singleton ' '
+    <> showb d
+    <> singleton ' '
+    <> showb e
+showbCS6
+  :: (TextShow a, TextShow b, TextShow c, TextShow d, TextShow e, TextShow f)
+  => Text
+  -> a
+  -> b
+  -> c
+  -> d
+  -> e
+  -> f
+  -> Builder
+showbCS6 n a b c d e f =
+  fromLazyText n
+    <> singleton ' '
+    <> showb a
+    <> singleton ' '
+    <> showb b
+    <> singleton ' '
+    <> showb c
+    <> singleton ' '
+    <> showb d
+    <> singleton ' '
+    <> showb e
+    <> singleton ' '
+    <> showb f
