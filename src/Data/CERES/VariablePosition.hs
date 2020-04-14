6f7f7e1a4cbf328ca85f9d6b57357efdb4a70e7f
YongJoon Joe <developer@quietjoon.net>
2020-04-14 21:12:57 +0900

    Add a VariableIndex series for partial `NKey` matching

--- src/Data/CERES/VariablePosition.hs
+++ src/Data/CERES/VariablePosition.hs
@@ -55,32 +55,41 @@ instance TextShow VariablePlace where
 
 
 data VariableIndex
-  = VII Idx | VIN NKey | VIIT Idx Time | VINT NKey Time
-  | VIIRI Idx [Idx] | VINRI NKey [Idx] | VIIRIT Idx [Idx] Time | VINRIT NKey [Idx] Time
+  = VII Idx | VIN NKey | VIpN NKey | VIIT Idx Time | VINT NKey Time | VIpNT NKey Time
+  | VIIRI Idx [Idx] | VINRI NKey [Idx] | VIpNRI NKey [Idx]
+  | VIIRIT Idx [Idx] Time | VINRIT NKey [Idx] Time | VIpNRIT NKey [Idx] Time
   | VIV Value | VIAtom | VINull
-  | PVII Idx | PVIN NKey | PVIT Time
-  | PVIIRI Idx [Idx] | PVINRI NKey [Idx] | PVIIRIT Idx [Idx] Time | PVINRIT NKey [Idx] Time
+  | PVII Idx | PVIN NKey | PVIpN NKey | PVIT Time
+  | PVIIRI Idx [Idx] | PVINRI NKey [Idx] | PVIpNRI NKey [Idx]
+  | PVIIRIT Idx [Idx] Time | PVINRIT NKey [Idx] Time | PVIpNRIT NKey [Idx] Time
   deriving (Eq, Ord)
 
 instance Show VariableIndex where
   show = TL.unpack . showtl
 
 instance TextShow VariableIndex where
-  showb (VII idx                 )  = showb1 "VII" idx
-  showb (VIN nKey                )  = showb1 "VIN" nKey
-  showb (VIIT  idx  time         )  = showb2 "VIIT" idx time
-  showb (VINT  nKey time         )  = showb2 "VINT" nKey time
-  showb (VIIRI idx  indices      )  = showb2 "VIIRI" idx indices
-  showb (VINRI nKey indices      )  = showb2 "VINRI" nKey indices
-  showb (VIIRIT idx  indices time)  = showb3 "VIIRIT" idx indices time
-  showb (VINRIT nKey indices time)  = showb3 "VINRIT" nKey indices time
-  showb (VIV value               )  = showb1 "VIV" value
-  showb VIAtom                      = fromLazyText "VIAtom"
-  showb VINull                      = fromLazyText "VINull"
-  showb (PVII idx                 ) = showb1 "PVII" idx
-  showb (PVIN nKey                ) = showb1 "PVIN" nKey
-  showb (PVIT time                ) = showb1 "PVIT" time
-  showb (PVIIRI idx  indices      ) = showb2 "PVIIRI" idx indices
-  showb (PVINRI nKey indices      ) = showb2 "PVINRI" nKey indices
-  showb (PVIIRIT idx  indices time) = showb3 "PVIIRIT" idx indices time
-  showb (PVINRIT nKey indices time) = showb3 "PVINRIT" nKey indices time
+  showb (VII  idx                 )  = showb1 "VII" idx
+  showb (VIN  nKey                )  = showb1 "VIN" nKey
+  showb (VIpN nKey                )  = showb1 "VIpN" nKey
+  showb (VIIT   idx  time         )  = showb2 "VIIT" idx time
+  showb (VINT   nKey time         )  = showb2 "VINT" nKey time
+  showb (VIpNT  nKey time         )  = showb2 "VIpNT" nKey time
+  showb (VIIRI  idx  indices      )  = showb2 "VIIRI" idx indices
+  showb (VINRI  nKey indices      )  = showb2 "VINRI" nKey indices
+  showb (VIpNRI nKey indices      )  = showb2 "VIpNRI" nKey indices
+  showb (VIIRIT  idx  indices time)  = showb3 "VIIRIT" idx indices time
+  showb (VINRIT  nKey indices time)  = showb3 "VINRIT" nKey indices time
+  showb (VIpNRIT nKey indices time)  = showb3 "VIpNRIT" nKey indices time
+  showb (VIV value                )  = showb1 "VIV" value
+  showb VIAtom                       = fromLazyText "VIAtom"
+  showb VINull                       = fromLazyText "VINull"
+  showb (PVII  idx                 ) = showb1 "PVII" idx
+  showb (PVIN  nKey                ) = showb1 "PVIN" nKey
+  showb (PVIpN nKey                ) = showb1 "PVIpN" nKey
+  showb (PVIT  time                ) = showb1 "PVIT" time
+  showb (PVIIRI  idx  indices      ) = showb2 "PVIIRI" idx indices
+  showb (PVINRI  nKey indices      ) = showb2 "PVINRI" nKey indices
+  showb (PVIpNRI nKey indices      ) = showb2 "PVIpNRI" nKey indices
+  showb (PVIIRIT  idx  indices time) = showb3 "PVIIRIT" idx indices time
+  showb (PVINRIT  nKey indices time) = showb3 "PVINRIT" nKey indices time
+  showb (PVIpNRIT nKey indices time) = showb3 "PVIpNRIT" nKey indices time
